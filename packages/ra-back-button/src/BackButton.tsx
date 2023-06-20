996ea4384692156ad3eaf393404bb8cc1dc9822b
Matteo Saloni <matteo.saloni@gmail.com>
2023-06-20 15:48:01 +0200

    add back button package

---
+++ packages/ra-back-button/src/BackButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, ButtonProps } from 'react-admin';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+
+export const BackButton = (props: ButtonProps) => {
+    const navigate = useNavigate();
+    const { label = 'ra.action.back', ...rest } = props;
+    return (
+        <Button
+            label={label}
+            startIcon={ArrowBackIcon}
+            onClick={() => navigate(-1)}
+            {...rest}
+        ></Button>
+    );
+};
