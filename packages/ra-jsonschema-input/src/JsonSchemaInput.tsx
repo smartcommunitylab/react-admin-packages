dca7fb2e19eca458e49e8aa6b707892c008bf48a
Nawaz Khurshid <nawaz1981@gmail.com>
2023-10-04 14:37:59 +0200

    Added ra-jsonschema-input component

---
+++ packages/ra-jsonschema-input/src/JsonSchemaInput.tsx
@@ -0,0 +1,46 @@
+import { useInput, InputProps } from 'react-admin';
+import Form from '@rjsf/mui';
+import validator from '@rjsf/validator-ajv8';
+
+export const JsonSchemaInput = (props: JsonSchemaFormatInputProps) => {
+    const { schema, uiSchema, onBlur, onChange } = props;
+
+    const {
+        field,
+        fieldState: { isTouched, error },
+        formState: { isSubmitted },
+    } = useInput({
+        onChange,
+        onBlur,
+        ...props,
+    });
+
+    const update = (data: any) => {
+        field.onChange(data);
+    };
+
+    return (
+        <>
+            <Form
+                schema={schema}
+                uiSchema={uiSchema}
+                formData={field.value}
+                validator={validator}
+                onChange={(e: any) => update(e.formData)}
+                omitExtraData={true}
+            >
+                <div>
+                    <button hidden type="submit"></button>
+                </div>
+            </Form>
+        </>
+    );
+};
+
+export type JsonSchemaFormatInputProps = InputProps & {
+    schema?: any;
+    uiSchema?: any;
+};
+
+
+export default JsonSchemaInput;
