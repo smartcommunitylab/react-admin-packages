b2b2dafcc66be9b3a1821e4bfd091fc493083913
Matteo Saloni <matteo.saloni@gmail.com>
2023-11-27 14:54:24 +0100

    update json schema field render - wip

--- packages/ra-jsonschema-input/src/JsonSchemaField.tsx
+++ packages/ra-jsonschema-input/src/JsonSchemaField.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import { useRecordContext, FieldProps } from 'react-admin';
 import validator from '@rjsf/validator-ajv8';
-import { RJSFSchema, UiSchema } from '@rjsf/utils';
+import {
+    BaseInputTemplateProps,
+    FormContextType,
+    GenericObjectType,
+    RJSFSchema,
+    StrictRJSFSchema,
+    UiSchema,
+} from '@rjsf/utils';
 import { Form } from '@rjsf/mui';
 import { get } from 'lodash';
 import { useRJSchema } from './utils';
+import BaseInputTemplate from '@rjsf/mui/lib/BaseInputTemplate';
+
+//TODO add css or update template
+// .RaJsonSchemaField-Form .MuiGrid-root > .MuiGrid-item {
+//     padding-top: 0;
+//     margin-bottom: 0 !important;
+// }
+
+//patch base template for custom field rendering
+function ReadOnlyBaseFieldTemplate<
+    T = any,
+    S extends StrictRJSFSchema = RJSFSchema,
+    F extends FormContextType = any
+>(props: BaseInputTemplateProps<T, S, F>) {
+    return BaseInputTemplate({
+        ...props,
+        variant: 'standard',
+        InputProps: {
+            readOnly: true,
+            disableUnderline: true,
+        },
+    });
+}
 
 export const JsonSchemaField = (props: JsonSchemaFieldProps) => {
-    const { schema, uiSchema = {}, label, resource, source } = props;
+    const {
+        schema,
+        uiSchema = {},
+        label,
+        resource,
+        source,
+        forceReadOnly = false,
+    } = props;
 
     const record = useRecordContext(props);
     const value = get(record, source);
@@ -20,6 +57,12 @@ export const JsonSchemaField = (props: JsonSchemaFieldProps) => {
         title: label && typeof label === 'string' ? label : undefined,
     });
 
+    //cleanup required for view-only render
+    const rSchema: GenericObjectType = rjsSchema as GenericObjectType;
+    if ('required' in rSchema) {
+        rSchema['required'] = [];
+    }
+
     return (
         <Form
             tagName={'div'}
@@ -29,9 +72,10 @@ export const JsonSchemaField = (props: JsonSchemaFieldProps) => {
             validator={validator}
             omitExtraData={true}
             showErrorList={false}
-            disabled
-            readonly
             noValidate
+            readonly={forceReadOnly}
+            templates={{ BaseInputTemplate: ReadOnlyBaseFieldTemplate }}
+            className="RaJsonSchemaField-Form"
         >
             <></>
         </Form>
@@ -43,6 +87,7 @@ export interface JsonSchemaFieldProps<
 > extends FieldProps<RecordType> {
     schema: RJSFSchema | object | string;
     uiSchema?: UiSchema | object | string;
+    forceReadOnly?: boolean;
 }
 
 export default JsonSchemaField;
